e7dda275e281a0670fdc66c4e3743eb842ced496
PHPHellCoder <38293783+PHPHellCoder@users.noreply.github.com>
2024-10-18 17:46:28 +0200

    kek

---
+++ config.ts
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  // Your web app's Firebase configuration here
+  // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
+  apiKey: "AIzaSyCgRfuX4NRFWGzBlsXiBw4diynSinNLc7k",
+  authDomain: "tett-54374.firebaseapp.com",
+  projectId: "tett-54374",
+  storageBucket: "tett-54374.appspot.com",
+  messagingSenderId: "229137545385",
+  appId: "1:229137545385:web:1f6606f8175403947551b8"
+};
+
+export const vapidKey = 'BDCshRxsr9PdktNUHskeDY_pYxE335vPsp90ZIMep5HezjOwbP8h8CSqEMqzJ-BTKQardlgse3Tmkk7AB9oWCaI';
